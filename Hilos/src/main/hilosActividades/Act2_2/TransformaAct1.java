113af26a2b3d4382727fa11adda5bdfa800841cb
olalla103 <olalla.lopeznavarro-casas@iesvalleinclan.es>
2024-10-14 11:59:59 +0200

    adicion

--- Hilos/src/main/hilosActividades/Act2_2/TransformaAct1.java
+++ Hilos/src/main/hilosActividades/Act2_2/TransformaAct1.java
@@ -13,13 +13,13 @@ public class TransformaAct1 implements Runnable {
     @Override
     public void run() {
         try {
+            // Espera un tiempo basado en el ID del hilo actual
             Thread.sleep(Thread.currentThread().getId() * 100);
+            // Imprime el ID del hilo actual
             System.out.println("Hola Mundo " + this.getPalabra() + " " + Thread.currentThread().getId());
-
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-
     }
 
     public String getPalabra() {
